b8f349711a9d8c14ad871a6830cd5476863e6857
MVDam <47767573t@iespoblenou.org>
2016-04-18 23:01:26 +0200

    - metodo de guardar colecciones

--- src/DAO/DAOdeDAO.java
+++ src/DAO/DAOdeDAO.java
@@ -1,5 +1,10 @@
 package DAO;
 
+import factory.Empresa;
+import org.xmldb.api.base.XMLDBException;
+
+import java.awt.*;
+
 /**
  * Created by Moises on 14/04/2016.
  */
@@ -9,18 +14,29 @@ public class DAOdeDAO {
     private static final String PORT = "8080";
     private static String adminUsername = "admin";
     private static String adminPassword = "dionis";
+    private static String dbPath = "C:\\Users\\Moises\\Desktop\\Git\\M06DAOsobreDAO\\src";
+
+    //Variables de base de datos
+    String[] colecciones = {"clients", "empleats", "catalegs", "factures"};
 
 
-    public DAOdeDAO() {
+    DAO1 subDAO = new DAO1(IP, PORT);
+    Empresa empresaDB;
+
+    public DAOdeDAO() throws XMLDBException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+
+        //Parte inicial creamos la bbdd y sus colecciones específicas
+        for (int i = 0; i < colecciones.length; i++) {
+            guardarColeccion(colecciones[i]);
+        }
 
 
 
     }
 
-    public void guardarColeccion(){
-
-
-
+    public void guardarColeccion(String nombreCol) throws XMLDBException, ClassNotFoundException, InstantiationException
+            , IllegalAccessException {
+        subDAO.createCollection(adminUsername, adminPassword, nombreCol);
     }
 
 
